783b8c17a1025c40abddbe35fe345d70674223b8
yahya-Boussakla <yahyaboussakla@gmail.com>
2024-09-25 11:40:36 +0100

    presentation with switch

--- realisation/index.php
+++ realisation/index.php
@@ -15,81 +15,91 @@
     {
       echo chr(27) . chr(91) . 'H' . chr(27) . chr(91) . 'J';
       echo "bienvenu dans bilbliotheque manager program\n\n";
-    
+      $exitAutheurs = false;
       $exitProgram = false;
+      $exitLivre = false;
       while (!$exitProgram) {
         echo "+------------------------------------+\n";
         echo "|        Books Management            |\n";
         echo "|------------------------------------|\n";
         echo "|    Please choose an action:        |\n";
-        echo "|------------------------------------| \n";
+        echo "|------------------------------------|\n";
         echo "| [a] - autheurs                     |\n";
         echo "| [l] - livres                       |\n";
         echo "| [exit] - Exit the program          |\n";
         echo "+------------------------------------+\n\n";
-    
         $class = askQuestion("Your choice: ");
         switch (strtolower( $class)) {
-            case 'a':
-                echo "+------------------------------------+\n";
-                echo "|        Books Management            |\n";
-                echo "|------------------------------------|\n";
-                echo "|    Please choose an action:        |\n";
-                echo "|------------------------------------| \n";
-                echo "| [l] - list d'autheurs              |\n";
-                echo "| [a] - ajouter autheur              |\n";
-                echo "| [exit] - Exit the program          |\n";
-                echo "+------------------------------------+\n\n";
-                $autheuraction = askQuestion("Your choice: ");
-                switch (strtolower(  $autheuraction)) {
-                    case 'l':
-                    $autheurPresentation = new AutheurPresentation();
-                    $autheurPresentation->viewAutheurs();
-                    break;
-                    case 'a':    
-                        $bookPresentation = new AutheurPresentation();
-                        $bookPresentation->ajoutAutheur();
-                    break;
-                    case 'exit':
-                      $exitProgram = true;
+          case 'a':
+            while (!$exitAutheurs) {   
+                  echo "+------------------------------------+\n";
+                  echo "|        Books Management            |\n";
+                  echo "|------------------------------------|\n";
+                  echo "|    Please choose an action:        |\n";
+                  echo "|------------------------------------|\n";
+                  echo "| [l] - list d'autheurs              |\n";
+                  echo "| [a] - ajouter autheur              |\n";
+                  echo "| [exit] - Exit the program          |\n";
+                  echo "+------------------------------------+\n\n";
+                  $autheuraction = askQuestion("Your choice: ");
+                  switch (strtolower(  $autheuraction)) {
+                      case 'l':
+                      $autheurPresentation = new AutheurPresentation();
+                      $autheurPresentation->viewAutheurs();
                       break;
-              
-                    default:
-                      echo "Invalid choice. Please try again.\n";
+                      case 'a':    
+                          $bookPresentation = new AutheurPresentation();
+                          $bookPresentation->ajoutAutheur();
                       break;
+                      case 'exit':
+                        $exitAutheurs = true;
+                        break;
+                
+                      default:
+                        echo "Invalid choice. Please try again.\n";
+                        break;
+                  }
+                  }
+                break;
+              case 'l':
+                while (!$exitLivre) {
+                      echo "+------------------------------------+\n";
+                      echo "|        Books Management            |\n";
+                      echo "|------------------------------------|\n";
+                      echo "|    Please choose an action:        |\n";
+                      echo "|------------------------------------|\n";
+                      echo "| [l] - list des livres              |\n";
+                      echo "| [a] - ajouter livre                |\n";
+                      echo "| [exit] - Exit the program          |\n";
+                      echo "+------------------------------------+\n\n";
+                      $livreaction = askQuestion("Your choice: ");
+                      switch (strtolower(  $livreaction)) {
+                          case 'l':
+                          $livrePresentation = new LivrePresentation();
+                          $livrePresentation->viewlistLivres();
+                          break;
+                          case 'a':
+                              $livrePresentation = new LivrePresentation();
+                              $livrePresentation->ajoutLivre();
+                          break;
+                          case 'exit':
+                            $exitLivre = true;
+                            break;
+                    
+                          default:
+                            echo "Invalid choice. Please try again.\n";
+                            break;
+                      } 
                 }
-            case 'l':
-                    echo "+------------------------------------+\n";
-                    echo "|        Books Management            |\n";
-                    echo "|------------------------------------|\n";
-                    echo "|    Please choose an action:        |\n";
-                    echo "|------------------------------------| \n";
-                    echo "| [l] - list des livres              |\n";
-                    echo "| [a] - ajouter livre                |\n";
-                    echo "| [exit] - Exit the program          |\n";
-                    echo "+------------------------------------+\n\n";
-                    $livreaction = askQuestion("Your choice: ");
-                    switch (strtolower(  $livreaction)) {
-                        case 'l':
-                        $livrePresentation = new LivrePresentation();
-                        $livrePresentation->viewlistLivres();
-                        break;
-                        case 'a':    
-                            $livrePresentation = new LivrePresentation();
-                            $livrePresentation->ajoutLivre();
-                        break;
-                        case 'exit':
-                          $exitProgram = true;
-                          break;
-                  
-                        default:
-                          echo "Invalid choice. Please try again.\n";
-                          break;
-                    }    
-        
-          }
-          echo "Exiting the program. Goodbye!\n";
+              break;
+
+              case 'exit':
+                $exitProgram = true;
+                break;
+            echo "Exiting the program. Goodbye!\n";
         }
+
+      }
 }
     
     library_management();
