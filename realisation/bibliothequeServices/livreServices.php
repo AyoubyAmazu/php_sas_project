6d0d319a4c9475213b81291efa0af84816febaae
ChabbehAymen <chabbehaymensolicode@gmail.com>
2024-09-25 16:19:52 +0100

    added Livre class

--- realisation/bibliothequeServices/livreServices.php
+++ realisation/bibliothequeServices/livreServices.php
@@ -1,5 +1,5 @@
 <?php
-    require_once (dirname(__FILE__) ."/../bibliothequeDAO/dataManager.php");
+    require_once (dirname(__FILE__) ."/../dataAccess/DataManager.php");
     class LivreServices{
         private $data;
 
